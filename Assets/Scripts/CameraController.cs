2264692f3958c89d2ca0aafc3199a02e5fcb49bd
fjortegan <fjortegan@gmail.com>
2024-01-26 14:21:46 +0100

    keys test

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] List<GameObject> cameras;
+    public List<InputAction> actions;
     [SerializeField] float cycleTime;
+    
     private int _currentCamera;
+    private KeyboardInput _keyboardInput;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        _keyboardInput = new KeyboardInput();
+        _keyboardInput.Rooms.Enable();
+        _keyboardInput.Rooms.Camera1.performed += 
+            (InputAction.CallbackContext cb) => {
+                SetCamera(0); 
+            }; 
+        _keyboardInput.Rooms.Camera2.performed += 
+            (InputAction.CallbackContext cb) => {
+                SetCamera(1); 
+            }; 
+        _keyboardInput.Rooms.Camera3.performed += 
+            (InputAction.CallbackContext cb) => {
+                SetCamera(2); 
+            }; 
+        _keyboardInput.Rooms.Camera4.performed += 
+            (InputAction.CallbackContext cb) => {
+                SetCamera(3); 
+            }; 
+        _keyboardInput.Rooms.Camera5.performed += 
+            (InputAction.CallbackContext cb) => {
+                SetCamera(4); 
+            }; 
+        actions[0].performed += (InputAction.CallbackContext cb) => {
+            SetCamera(0); 
+        };
         _currentCamera = 0;
-        SetCamera();
-        StartCoroutine(CycleCameras());
+        SetCamera(0);
+        //StartCoroutine(CycleCameras());
     }
 
     // Update is called once per frame
@@ -26,11 +55,11 @@ public class CameraController : MonoBehaviour
     {
         _currentCamera++;
         _currentCamera%= cameras.Count;
-        SetCamera();
+        SetCamera(_currentCamera);
     }
 
-    void SetCamera()
-    {
+    void SetCamera(int cameraNumber) {
+        _currentCamera = cameraNumber;
         foreach (var camera in cameras) {
             camera.SetActive(false);
         }
