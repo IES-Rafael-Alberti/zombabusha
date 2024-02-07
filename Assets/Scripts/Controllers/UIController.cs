574d9fd0d0b7a228159c7a8b723d8073a913a6b5
Rubus <66851315+Rubusderg@users.noreply.github.com>
2024-02-07 12:28:14 +0100

    UI controller update

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour
+{
     [SerializeField] private float flashTime;
 
     [SerializeField] private AudioSource fallSound;
     // Start is called before the first frame update
-    void Start() {
+
+    public Image fillImage; //Rubus (Image for the RageBar)
+    public static float fillAmount = 0f;
+
+    void Start()
+    {
         EventManager.SonEventsList[SonEvents.fall] += () => {
             StartCoroutine(SonFall());
         };
@@ -16,14 +23,22 @@ public class UIController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    #region Son Events
-    IEnumerator SonFall() {
+
+    IEnumerator SonFall()
+    {
+        IncreaseRageFill(5f); //Rubus (Fills bar completely TODO fix)
         GameManager.Instance.cameratexture.enabled = true;
         fallSound.Play();
         yield return new WaitForSeconds(flashTime);
         GameManager.Instance.cameratexture.enabled = false;
     }
-    #endregion
+
+    public void IncreaseRageFill(float increaseAmount) //Rubus (Function for increasing Rage)
+    {
+        fillAmount += increaseAmount / 100f;
+        fillAmount = Mathf.Max(fillAmount, 0);
+        fillImage.fillAmount = fillAmount;
+    }
 }
