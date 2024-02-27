19e6bb5afc5b210e8d567a344aff41d8de3d0e34
DeividVA <david.verdugoabad@gmail.com>
2024-02-27 15:22:58 +0100

    Collision and Freeze Try

---
+++ Assets/Scripts/Minigames/DartsController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DartsController : MonoBehaviour
+{
+
+    public GameObject dart;
+    public GameObject bullseye;
+    [SerializeField] private float bulletOffset;
+    [SerializeField] private Vector3 bulletForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shot();
+            
+        }
+
+    }
+
+    private void Shot()
+    {
+        //Debug.Log("Estás pulsando click izquierdo!");
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        GameObject bullet = Instantiate(dart, gameObject.transform);
+        bullet.transform.position = new Vector3(bullseye.transform.position.x, bullseye.transform.position.y, bullseye.transform.position.z + bulletOffset);
+        bullet.transform.localScale = Vector3.one * 250;
+        bullet.GetComponent<Rigidbody>().AddForce(bulletForce, ForceMode.Impulse);
+
+
+        Destroy(bullet, 3f);
+    }
+
+
+}
