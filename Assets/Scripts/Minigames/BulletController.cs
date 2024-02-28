64a6fe6744834794c0ada199194551a7b63d4fc9
DeividVA <david.verdugoabad@gmail.com>
2024-02-28 11:36:38 +0100

    Aim n shot w/ mouse

--- Assets/Scripts/Minigames/BulletController.cs
+++ Assets/Scripts/Minigames/BulletController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
--- Assets/Scripts/Minigames/DartsController.cs
+++ Assets/Scripts/Minigames/DartsController.cs
@@ -10,16 +10,30 @@ public class DartsController : MonoBehaviour
     public GameObject bullseye;
     [SerializeField] private float bulletOffset;
     [SerializeField] private Vector3 bulletForce;
+    [SerializeField] private Camera camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        /* [2D Method test] 
+        Vector2 mouseWorldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mouseWorldPoint);*/
+
+
+        /* [3D Method test] 
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit)) {
+            Debug.Log(raycastHit.point);
+        }*/
+
+
 
         if (Input.GetButtonDown("Fire1"))
         {
@@ -31,12 +45,17 @@ public class DartsController : MonoBehaviour
 
     private void Shot()
     {
-        //Debug.Log("Estás pulsando click izquierdo!");
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         GameObject bullet = Instantiate(dart, gameObject.transform);
         bullet.transform.position = new Vector3(bullseye.transform.position.x, bullseye.transform.position.y, bullseye.transform.position.z + bulletOffset);
         bullet.transform.localScale = Vector3.one * 250;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            bullet.transform.position = raycastHit.point;
+        }
+
+
         bullet.GetComponent<Rigidbody>().AddForce(bulletForce, ForceMode.Impulse);
 
 
